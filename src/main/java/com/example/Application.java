eaf60a2a0e92617e39122bfa6d948a571071c77d
Guillermo Calvo <guillermo@guillermo.dev>
2024-06-15 14:19:52 +0200

    Simple pet store implementation

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,12 +3,36 @@ package com.example;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.*;
 import io.swagger.v3.oas.annotations.info.*;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @OpenAPIDefinition(
-    info = @Info(
-            title = "result-example-micronaut",
-            version = "0.0"
-    )
+        info = @Info(
+                title = "Result Petstore",
+                description = "Example Micronaut application using results",
+                version = "1.0",
+                contact = @Contact(
+                        url = "https://guillermo.dev",
+                        name = "Guillermo Calvo",
+                        email = "hello@guillermo.dev"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Find out more about Result library for Java",
+                url = "https://dev.leakyabstractions.com/result/"
+        ),
+        tags = @Tag(
+                name = "pet",
+                description = "Everything about your Pets"
+        ),
+        servers = @Server(
+                url = "http://localhost:8080",
+                description = "DEV"
+        )
 )
 public class Application {
 
