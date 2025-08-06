0385da26e6065f90eca8657935443954ff866f7f
Guillermo Calvo <guillermo@guillermo.dev>
2025-08-06 17:19:28 +0200

    Upgrade Gradle shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.micronaut.application") version "4.5.4"
+    id("com.gradleup.shadow") version "8.3.7"
     id("io.micronaut.aot") version "4.5.4"
 }
 
